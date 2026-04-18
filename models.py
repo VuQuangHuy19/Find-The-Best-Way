9d5b537785422fa17a4eff9a2c602f3a3b759d82
hlk9 <hoanglong.cmb@gmail.com>
2026-04-18 22:41:06 +0700

    add model for node, edge

---
+++ models.py
@@ -0,0 +1,12 @@
+class MapNode:
+    def __init__(self, node_id, lat, lon):
+        self.id = node_id
+        self.lat = lat
+        self.lon = lon
+
+class MapEdge:
+    def __init__(self, u, v, length, oneway=False):
+        self.u = u  # ID node bắt đầu
+        self.v = v  # ID node kết thúc
+        self.length = length
+        self.oneway = oneway
